5de27c2c5576b8eca6117284a72e65e209988c45
huy2123 huy2123 <huy299295@gmail.com>
2023-04-02 15:26:25 +0700

    first commit

---
+++ tet/tet.js
@@ -0,0 +1,25 @@
+function scrollToElement2() {
+    var element = document.getElementById("text2");
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+  
+  function scrollToElement3() {
+    var element = document.getElementById("text3");
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+    
+  function scrollToElement4() {
+    var element = document.getElementById("text4");
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+    
+  function scrollToElement5() {
+    var element = document.getElementById("text5");
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+    
+  function scrollToElement6() {
+    var element = document.getElementById("text6");
+    element.scrollIntoView({ behavior: "smooth" });
+  }
+    
